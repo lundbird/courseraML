38d0f6c424e016b4956357a09ccc157a56e64e5d
alex <alex.lundberg@gmail.com>
2018-06-21 00:12:31 -0500

    calling function adds bias column to X

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -12,7 +12,7 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-J = 1/(2*m) * sum((X*theta - y).^2) + lambda/(2*m) * sum(theta.^2);
+J = 1/(2*m) * sum((X*theta - y).^2) + lambda/(2*m) * sum(theta(2:end, :).^2);
 
 
 % =========================================================================
