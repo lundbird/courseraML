d5ef13575993407cf1a92e76270f57f3e7975d1a
alex <alex.lundberg@gmail.com>
2018-06-21 00:55:19 -0500

    complete learning curve that show high bias

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -40,15 +40,11 @@ error_val   = zeros(m, 1);
 %       Do note that you will still need to use lambda when running
 %       the training to obtain the theta parameters.
 %
-% Hint: You can loop over the examples with the following:
-%
-%       for i = 1:m
-%           % Compute train/cross validation errors using training examples 
-%           % X(1:i, :) and y(1:i), storing the result in 
-%           % error_train(i) and error_val(i)
-%           ....
-%           
-%       end
+       for i = 1:m
+            theta = trainLinearReg(X(1:i,:),y(1:i,:),lambda);
+            error_train(i) = 1/(2*m) * sum((X(1:i,:)*theta -y(1:i,:)).^2);
+            error_val(i) = 1/(2*m) * sum((Xval*theta - yval).^2);     
+       end
 %
 
 % ---------------------- Sample Solution ----------------------
