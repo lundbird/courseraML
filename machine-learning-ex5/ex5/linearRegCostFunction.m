ae611e939ef8252017c272fc4d015450a94a9e56
alex <alex.lundberg@gmail.com>
2018-06-21 00:02:01 -0500

    complete cost function

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -12,22 +12,7 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
-
-
-
-
-
-
-
-
-
-
+J = 1/(2*m) * sum((X*theta - y).^2) + lambda/(2*m) * sum(theta.^2);
 
 
 % =========================================================================
