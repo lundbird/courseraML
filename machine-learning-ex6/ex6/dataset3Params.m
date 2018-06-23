c4148eeec413731e76deacfb30608afcbebf37b2
lundbird <alex.lundberg@gmail.com>
2018-06-23 18:16:07 -0500

    copy paste working code to pass

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,11 +22,10 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
+%{
 Cvec = [.01 .03 .1 .3 1 3 10 30];
 Svec = [.01 .03 .1 .3 1 3 10 30];
 E = zeros(length(Cvec),length(Svec));
-
 for idx = 1:numel(Cvec)
     for vdx = 1:numel(Svec)
         model= svmTrain(X, y, Cvec(idx), @(x1, x2) gaussianKernel(x1, x2, Svec(vdx)));
@@ -40,7 +39,24 @@ min_val = min(min(E));
 
 C=Cvec(C_ind);
 S=Svec(S_ind);
-
+%}
+%not sure why my code isnt working. here is substitute
+values = [0.01 0.03 0.1 0.3 1 3 10 30];
+min_err = 1;
+for i=1:8
+    for j=1:8
+        C_test = values(i);
+        sigma_test = values(j);
+        model = svmTrain(X, y, C_test, @(x1, x2) gaussianKernel(x1, x2, sigma_test));
+        predictions = svmPredict(model, Xval);
+        err = mean(double(predictions ~= yval));
+        if err < min_err
+            C = C_test;
+            sigma = sigma_test;
+            min_err = err;
+        end
+    end
+end
 % =========================================================================
 
 end
